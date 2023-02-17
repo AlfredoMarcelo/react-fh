9f3790545145db8702bb3b692cbd4af6c47c6a99
alfredo <alfredo.supanta.26@gmail.com>
2023-02-16 22:11:15 -0300

    se culmina intro js

---
+++ src/bases/12-ternario.js
@@ -0,0 +1,11 @@
+const activo = false;
+
+// Evalua si es true o false () ? =true : =false; 
+const mensaje = (activo) ? "activo": "inactivo";
+
+// Solo evalua si es true, ejecutara la accion
+// Si no cumple, devuelve false
+const mensaje1 = activo && "activo"
+
+console.log(mensaje)
+console.log(mensaje1)
